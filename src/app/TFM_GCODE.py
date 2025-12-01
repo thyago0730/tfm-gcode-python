4c1609218717db0eed042adbf96d7ec6d4969143
TFM User <user@example.com>
2025-12-01 10:49:42 -0300

    release: APP_VERSION 1.0.9 e verificação de versão no workflow

--- src/app/TFM_GCODE.py
+++ src/app/TFM_GCODE.py
@@ -1501,7 +1501,7 @@ class SettingsWindow(Toplevel):
             self.parent_app.show_notification(f'Erro ao importar: {e}', 'error')
 
 # Versão do aplicativo para controle de atualização
-APP_VERSION = "1.0.8"
+APP_VERSION = "1.0.9"
 
 # --- CLASSE PRINCIPAL ---
 class TFM_GCODE:
